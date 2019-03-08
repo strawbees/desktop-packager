b0a81a986838ae00cd967aad9ea9111bc0414ed9
Murilo Polese <murilopolese@gmail.com>
2019-03-08 16:43:34 +0000

    Using an external package to solve https and redirect downloads

--- utils/download.js
+++ utils/download.js
@@ -1,29 +1,16 @@
-const http = require('http')
-const https = require('https')
 const fs = require('fs')
-// http.globalAgent = require("caw")(process.env.npm_config_proxy || process.env.http_proxy || process.env.HTTP_PROXY);
+const request = require('request')
 
 module.exports = async (url, path) => {
-	const httpx = url.indexOf('https://') == 0 ? https : http
 	await new Promise((resolve, reject) => {
-		httpx.get(url, (response) => {
-			// Check if response code is not "301: Moved Permanently"
-			if (response.statusCode == 301) {
-				return reject(new Error(`Response returned with status code ${response.statusCode}: ${response.statusMessage}`))
-			}
-			const file = fs.createWriteStream(path)
-			response.pipe(file)
-			file.on('finish', () => {
-				file.close((err) => {
-					if (err) {
-						return reject(err)
-					}
-					return resolve()
-				})
+		request.get(url)
+			.on('error', async (err) => {
+				await fs.promises.unlink(path)
+				reject(err)
 			})
-		}).on('error', async (err) => {
-			await fs.promises.unlink(path)
-			reject(err)
-		})
+			.on('response', async (response) => {
+				resolve()
+			})
+			.pipe(fs.createWriteStream(path))
 	})
 }
