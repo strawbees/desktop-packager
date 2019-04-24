67d8c3f137ed0b62dc5678f33ae07c228db34d3a
Murilo Polese <murilopolese@gmail.com>
2019-04-24 11:51:23 +0100

    Recursively calculate size of files in a folder

---
+++ utils/folderSize.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises
+const path = require('path')
+const asyncForEach = require('./asyncForEach')
+
+/**
+ * Recursively calculate size of all files inside a folder
+ * @param {String} filePath - Path of folder to calculate (relative or absolute)
+ * @return {Number} Total size in bytes
+ */
+const readSizeRecursive = async (filePath) => {
+	const stats = await fs.stat(path.resolve(filePath))
+	if (stats.isDirectory()) {
+		let total = 0
+		let list = await fs.readdir(filePath)
+		await asyncForEach(list, async (dirItem) => {
+			total += await readSizeRecursive(
+				path.resolve(filePath, dirItem)
+			)
+		})
+		return total
+	} else {
+		return stats.size
+	}
+}
+
+module.exports = readSizeRecursive
